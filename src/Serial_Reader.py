e83af5077aa03257875542abaeddd39267830408
noahj9450hnson <LGJohnson11@gmail.com>
2023-02-06 16:26:52 -0800

    Update Serial_Reader.py

--- src/Serial_Reader.py
+++ src/Serial_Reader.py
@@ -3,10 +3,17 @@ import matplotlib.pyplot as plt
 
 def main():
     with serial.Serial ('COMx', 115200) as s_port:     
-        s_port.write ()
-        data_array.append(s_port.readline().replace(b" ", b"").strip().split(b","))
+        s_port.write (input('kp Value')
+        s_port.write (input('setPoint Value')
+   
+   while (True):              
+        try:
+        with serial.Serial ('COMx', 115200) as s_port:     
+            data_array.append(s_port.readline().replace(b" ", b"").strip().split(b","))
         
-    data_array = [[b'1',b'2'], [b'4',b'5', b'6'], ['hello' ,7], [], ['h', 'j', 'k']]
+        except UnicodeError: #run and see what error happens 
+            break
+                     
     float_array = []
     float_list = []
     
@@ -25,9 +32,11 @@ def main():
     
     x_data = [row[0] for row in float_array]
     y_data = [row[1] for row in float_array]
-
+    
     plt.plot(x_data, y_data)
     plt.suptitle('Proportional Control Response')
+    plt.xlabel('Time')
+    plt.ylabel('Position')
     plt.show()
 
 if __name__ == "__main__":
