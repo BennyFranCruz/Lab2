b56740eaf574353ff4ecc6817f0a2c60effc5bfa
noahj9450hnson <LGJohnson11@gmail.com>
2023-02-09 13:58:45 -0800

    Update Serial_Reader.py

--- src/Serial_Reader.py
+++ src/Serial_Reader.py
@@ -1,40 +1,40 @@
 import serial
 import matplotlib.pyplot as plt
+import time
 
 def main():
-    with serial.Serial ('COM3', baudrate=115200) as s_port:
-        #input1 = input('kp Value ')
+    with serial.Serial ('COM6', baudrate=115200, timeout=1000) as s_port:
+        print(s_port)
+        input1 = input('kp Value ')
+        input1 = bytes(input1, 'utf-8')
+        s_port.write(input1)
+        
+    x_data = []
+    y_data = []
+    
+    with serial.Serial ('COM6', 115200) as s_port:
         while (True):
-            s_port.write(b'2')
-            print("cycled")
-        
-        #x = s_port.readline()
-        #print(x)
-        
-    #x_data = []
-    #y_data = []
+            data_array = []
+            data_array.append(s_port.readline().replace(b" ", b"").strip().split(b","))
+            if data_array == "hello":#if its blank break out of the while loop
+                break
+            try:
+                x_data_float = float(data_array[0])
+                y_data_float = float(y_data_array[1])
+                x_data.append(x_data_float)
+                y_data.append(y_data_float)
+            except ValueError:
+                continue
+            except IndexError:
+                continue
+            except UnicodeError: #Set this to be the error that occurs when there is no data left
+                break
     
-    #with serial.Serial ('COM3', 115200) as s_port:
-    #    while (True):
-    #        data_array = []
-    #        data_array.append(s_port.readline().replace(b" ", b"").strip().split(b","))
-    #        try:
-    #            x_data_float = float(data_array[0])
-    #            y_data_float = float(y_data_array[1])
-    #            x_data.append(x_data_float)
-    #            y_data.append(y_data_float)
-    #        except ValueError:
-    #            continue
-    #        except IndexError:
-    #            continue
-    #        except UnicodeError: #Set this to be the error that occurs when there is no data left
-    #            break
-    
-    #plt.plot(x_data, y_data)
-    #plt.suptitle('Proportional Control Response')
-    #plt.xlabel('Time')
-    #plt.ylabel('Position')
-    #plt.show()
+    plt.plot(x_data, y_data)
+    plt.suptitle('Proportional Control Response')
+    plt.xlabel('Time')
+    plt.ylabel('Position')
+    plt.show()
 
 
 if __name__ == "__main__":
