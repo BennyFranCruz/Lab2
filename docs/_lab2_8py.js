49c99ac9a9b385d5cb3bc51fb9ad015ac2ea257f
BennyFranCruz <123684992+BennyFranCruz@users.noreply.github.com>
2023-02-10 16:03:08 -0800

    Doxygen

---
+++ docs/_lab2_8py.js
@@ -0,0 +1,4 @@
+var _lab2_8py =
+[
+    [ "main", "_lab2_8py.html#aa29f38bdc6221702245eea9b4fd2a94e", null ]
+];
---
+++ docs/dir_8726baa05077e4ba09b3b9fc1ed76e11.js
@@ -0,0 +1,9 @@
+var dir_8726baa05077e4ba09b3b9fc1ed76e11 =
+[
+    [ "boot.py", "boot_8py.html", null ],
+    [ "encoder_reader.py", "encoder__reader_8py.html", "encoder__reader_8py" ],
+    [ "Lab2.py", "_lab2_8py.html", "_lab2_8py" ],
+    [ "motor_driver.py", "motor__driver_8py.html", "motor__driver_8py" ],
+    [ "porportional_controller.py", "porportional__controller_8py.html", "porportional__controller_8py" ],
+    [ "Serial_Reader.py", "_serial___reader_8py.html", "_serial___reader_8py" ]
+];
